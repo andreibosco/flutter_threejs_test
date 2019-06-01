3c4398c94ae74ac7b36ea9b6d9b3d40ebd60d7f5
Andrei Bosco <andrei.torres@uoit.net>
2019-05-31 21:02:28 -0400

    loading suzanne using jaguar

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Test project for using threejs with flutter using webview_flutter.
 
 Currently it depends on a fork version of [webview_flutter](https://github.com/andreibosco/plugins) while [shaqian pull request](https://github.com/flutter/plugins/pull/1247) doesn't get merged.
+
+## Dependencies:
+- *webview_flutter*: provides webview support
+- *jaguar*: local webserver to host data and avoid CORS cross-origin errors
+- *jaguar_flutter_asset*: serves assets using jaguar on localhost:8080
+- *three.js*: (NOT a flutter/dart package) loaded using webview_flutter
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:jaguar/jaguar.dart';
+import 'package:jaguar_flutter_asset/jaguar_flutter_asset.dart';
 
-void main() => runApp(MaterialApp(home: WebViewExample()));
+void main() async {
+  final server = Jaguar();
+  server.addRoute(serveFlutterAssets());
+  await server.serve(logRequests: true);
+
+  // debug
+  server.log.onRecord.listen((r) => print(r));
+
+  runApp(MaterialApp(home: WebViewExample()));
+}
 
 class WebViewExample extends StatelessWidget {
   final Completer<WebViewController> _controller =
@@ -16,7 +27,7 @@ class WebViewExample extends StatelessWidget {
       ),
       body: Builder(builder: (BuildContext context) {
         return WebView(
-          initialUrl: 'assets/threejs.html',
+          initialUrl: 'http://localhost:8080/suzanne.html',
           javascriptMode: JavascriptMode.unrestricted,
           onWebViewCreated: (WebViewController webViewController) {
             _controller.complete(webViewController);
@@ -36,7 +47,7 @@ class WebViewExample extends StatelessWidget {
             return FloatingActionButton(
               onPressed: () async {
                 controller.data.evaluateJavascript(
-                    "cube.material.color.setHex('0x' + Math.floor(Math.random() * 16777215).toString(16));");
+                    "suzanne.scene.children[0].material.color.setHex('0x' + Math.floor(Math.random() * 16777215).toString(16));");
               },
               child: const Icon(Icons.refresh),
             );
